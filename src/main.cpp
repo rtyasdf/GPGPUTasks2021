4d5622bb867a4e008a23fda0b56d81d698f408ee
kirill <donthaveone>
2021-09-13 10:46:06 +0300

    made todo's

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 #include <iostream>
 #include <stdexcept>
-
+#include <map>
 
 template <typename T>
 std::string to_string(T value)
@@ -29,6 +29,34 @@ void reportError(cl_int err, const std::string &filename, int line)
 
 #define OCL_SAFE_CALL(expr) reportError(expr, __FILE__, __LINE__)
 
+std::map<std::string, std::map<int, std::string>> stringValues {
+    {"type", {{1, "DEFAULT"}, {2, "CPU"}, {4, "GPU"}, {8, "ACCELERATOR"}, {0xFFFFFFFF, "ALL"}}},
+    {"image support", {{0, "False"}, {1, "True"}}},
+    {"memory", {{0, "megabytes"}}}};
+
+template <typename T>
+void printDeviceInfo(std::string propertyString, cl_device_info deviceInfo, cl_device_id deviceID, int convert=0)
+{
+    size_t byteCount;
+    OCL_SAFE_CALL(clGetDeviceInfo(deviceID, deviceInfo, 0, nullptr, &byteCount));
+
+    size_t count = byteCount / sizeof(T);
+    if (!count)
+        return;
+
+    std::vector<T> deviceProperty(count);
+    OCL_SAFE_CALL(clGetDeviceInfo(deviceID, deviceInfo, byteCount, deviceProperty.data(), 0));
+
+    std::string value = to_string(deviceProperty.data());
+    if (convert == 1)
+        value = stringValues[propertyString][deviceProperty[0]];
+    else if (convert == 2)
+        value = to_string(deviceProperty[0]);
+    else if (convert == 3)
+         value = to_string(deviceProperty[0] >> 20) + " " + stringValues[propertyString][0];
+
+    std::cout << "        Device " + propertyString + ": " + value << std::endl;
+}
 
 int main()
 {
@@ -68,19 +96,35 @@ int main()
         // в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит, проверив код, понять, чем же вызвана данная ошибка (некорректным аргументом param_name)
         // Обратите внимание, что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines, такие как CL_DEVICE_TYPE_GPU и т.п.
 
+        /*
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, 23, 0, nullptr, &platformNameSize));
+        // Имя переменной выбрасываемой ошибки CL_INVALID_VALUE
+        */
+
         // TODO 1.2
         // Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
         std::vector<unsigned char> platformName(platformNameSize, 0);
-        // clGetPlatformInfo(...);
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), nullptr));
         std::cout << "    Platform name: " << platformName.data() << std::endl;
 
         // TODO 1.3
         // Запросите и напечатайте так же в консоль вендора данной платформы
+        size_t vendorNameSize;
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, 0, nullptr, &vendorNameSize));
+        
+        std::vector<unsigned char> vendorName(vendorNameSize, 0);
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, vendorNameSize, vendorName.data(), nullptr));
+        std::cout << "    Vendor name: " << vendorName.data() << std::endl;
+
 
         // TODO 2.1
         // Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
         cl_uint devicesCount = 0;
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &devicesCount));
 
+        std::vector<cl_device_id> devices(devicesCount);
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), nullptr));
+         
         for (int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex) {
             // TODO 2.2
             // Запросите и напечатайте в консоль:
@@ -88,6 +132,15 @@ int main()
             // - Тип устройства (видеокарта/процессор/что-то странное)
             // - Размер памяти устройства в мегабайтах
             // - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+
+
+            cl_device_id deviceID = devices[deviceIndex];
+
+            printDeviceInfo<unsigned char>("name", CL_DEVICE_NAME, deviceID);
+            printDeviceInfo<cl_device_type>("type", CL_DEVICE_TYPE, deviceID, 1);
+            printDeviceInfo<cl_ulong>("memory", CL_DEVICE_GLOBAL_MEM_SIZE, deviceID, 3);
+            printDeviceInfo<cl_bool>("image support", CL_DEVICE_IMAGE_SUPPORT, deviceID, 1);
+            printDeviceInfo<cl_uint>("parallel compute units", CL_DEVICE_MAX_COMPUTE_UNITS, deviceID, 2);
         }
     }
 
