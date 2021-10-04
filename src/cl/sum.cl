ac496f9d42f2dc9d938547e6dacd92ede9163450
kirill <donthaveone>
2021-10-04 14:46:09 +0300

    kernel for sum

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,19 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+__kernel void sum(__global const unsigned int* a,
+                  __global unsigned int* res,
+                  unsigned int n)
+{
+    const unsigned int local_index = get_local_id(0);
+    const unsigned int size = get_local_size(0);
+    unsigned int index = local_index;
+
+    for(int i = 0; i < n; i++){
+        res[local_index] += a[index];
+        index += size;
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,12 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include"cl/sum_cl.h"
+#include<vector>
+#include<iostream>
 
 
 template<typename T>
@@ -20,7 +26,7 @@ int main(int argc, char **argv)
     int benchmarkingIters = 10;
 
     unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
+    unsigned int n = 100 * 1000 * 1000;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(42);
     for (int i = 0; i < n; ++i) {
@@ -59,6 +65,45 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+        
+        ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum");
+        sum.compile();
+
+        unsigned int workGroupSize = 128;
+        unsigned int global_work_size = 1; // (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+        std::vector<unsigned int> res(workGroupSize, 0);  // сумма для каждого потока
+
+        while (as.size() % workGroupSize > 0) // дополняем до кратности размера рабочей группы
+            as.push_back(0);
+
+        // создаем буфферы на устройстве
+        gpu::gpu_mem_32u as_gpu, res_gpu;
+        as_gpu.resizeN(as.size());
+        res_gpu.resizeN(workGroupSize);
+        
+        as_gpu.writeN(as.data(), as.size());
+        res_gpu.writeN(res.data(), workGroupSize);
+
+        // исполняем kernel
+        sum.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                 as_gpu, res_gpu, as.size() / workGroupSize);
+
+        // считываем в память host'а
+        res_gpu.readN(res.data(), workGroupSize);
+        
+        unsigned int res_sum = 0; // итоговая сумма
+        for(int i=0; i < workGroupSize; i++) // проходим по частичным суммам, собраных потоками
+            res_sum += res[i];
+
+        // Проверяем корректность результатов
+        EXPECT_THE_SAME(reference_sum, res_sum, "GPU results should be equal to CPU results!");
     }
+
+    return 0;
 }
