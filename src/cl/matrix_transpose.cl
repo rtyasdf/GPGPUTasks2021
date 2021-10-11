24a5281ef8faeadc7a965cafdf925b3a030e4f49
kirill <donthaveone>
2021-10-11 15:36:48 +0300

    transpose

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,33 @@
-__kernel void matrix_transpose(...)
+#define WORKGROUP_SIZE 256
+
+__kernel void matrix_transpose(__global float* a, __global float* at, unsigned int K, unsigned int M)
 {
-    // TODO
+    __local float buf[WORKGROUP_SIZE];
+
+    unsigned int globalX = get_global_id(0);
+    unsigned int globalY = get_global_id(1);
+    unsigned int localX = get_local_id(0);
+    unsigned int localY = get_local_id(1);
+    unsigned int localSizeX = get_local_size(0);
+    unsigned int localSizeY = get_local_size(1);
+
+    unsigned int globalIndex = K * globalY + globalX;
+    unsigned int bufIndex = localSizeX * localX + (localX + localY) % localSizeX;
+
+    if (globalX < K && globalY < M)
+        buf[bufIndex] = a[globalIndex];
+    else
+        buf[bufIndex] = 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    unsigned int newIndex = localSizeX * localY + (localY + localX) % localSizeX;
+    
+    unsigned int groupOffset = M * (globalX / localSizeX) * localSizeX; 
+    unsigned int offsetY =  M * localY;
+    unsigned int offsetX = (globalY / localSizeY) * localSizeY;
+    unsigned int transposedIndex = groupOffset + offsetY + offsetX + localX;
+
+    if (globalX + (localY - localX) < K && globalY + (localX - localY) < M)
+        at[transposedIndex] = buf[newIndex];
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -31,8 +31,7 @@ int main(int argc, char **argv)
         as[i] = r.nextf();
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
-
-    /*
+    
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
@@ -42,21 +41,31 @@ int main(int argc, char **argv)
     ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
     matrix_transpose_kernel.compile();
 
+    
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
+            unsigned int work_group_size_x = 16;
+            unsigned int global_work_size_x = K;
+            while (global_work_size_x % work_group_size_x > 0)
+                global_work_size_x++;
+
+            unsigned int work_group_size_y = 16;
+            unsigned int global_work_size_y = M;
+            while (global_work_size_y % work_group_size_y > 0)
+                global_work_size_y++;
+
             // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
             // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
             // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
             // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
             // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
-
+            //matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x, global_work_size_y), as_gpu, as_t_gpu, K, M);
             t.nextLap();
         }
+    
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
     }
@@ -74,7 +83,6 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
 
     return 0;
 }
