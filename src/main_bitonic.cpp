ea3c55f76a3684a65ee25f85554b64ccd97531cf
kirill <donthaveone>
2021-11-22 07:31:02 +0300

    bitonic sort

--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -30,8 +30,9 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 10; // 10
+    int cpuBenchmarkingIters = 1;
+    unsigned int n = 32 * 1024 * 1024; // 32 * 1024 * 1024
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -42,15 +43,16 @@ int main(int argc, char **argv) {
     std::vector<float> cpu_sorted;
     {
         timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        for (int iter = 0; iter < cpuBenchmarkingIters; ++iter) {
             cpu_sorted = as;
             std::sort(cpu_sorted.begin(), cpu_sorted.end());
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << (n / 1000. / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+    
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
 
@@ -62,15 +64,22 @@ int main(int argc, char **argv) {
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
+            int limit = 7; // Ограничение по локальной памяти (2 ^ limit) * sizeof(float) байт
+
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
             unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            bitonic.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * (workGroupSize  >> 1);
+
+            for(int i=0; (1 << i) < n; i++){
+                int bound = (i >= limit) ? limit : i;
+                for(int j=i; j >= bound; j--)
+                    bitonic.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, j, limit, i + 1);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (n / 1000. / 1000) / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
     }
@@ -79,6 +88,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
